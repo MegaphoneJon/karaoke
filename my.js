5ceb08cf5d9ab3a8e5a5ed5c01e83d209f5af095
Jon goldberg <jon@palantetech.coop>
2017-03-25 16:58:50 +0000

    Pre-alpha AJAX code that searches the database

--- my.js
+++ my.js
@@ -1,5 +1,26 @@
-var options = {
-    valueNames: [ 'artist', 'song' ]
-};
+/*
+ $.get( "search.php?search=Daddy", function( response ) {
+  console.log( response );
+});
+*/
+$.ajax({
+  url: "search.php",
+  data: {
+    search: "Gasolina"
+  },
+  type: "GET",
+  dataType: "json",
+})
+.done(function( json ) {
+  $(".list").after(json);
+  temp = json;
+  $.each(json, function ( key, song) {
+    $(".list").append( "<li class=song>" );
+    $(".list").append( "<span class=artist>" + song.artist + "</span>" );
+    $(".list").append( "<span class=name>" + song.name + "</span>" );
+    $(".list").append( "</li>" );
 
-var userList = new List('karaoke', options);
+    console.log(song.artist);
+    console.log(song.name);
+  });
+});

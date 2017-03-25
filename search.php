074ac341627f24dcfbd7875ae86c786d03563d5b
Jon goldberg <jon@palantetech.coop>
2017-03-25 15:51:44 +0000

    Simple back-end search written in PHP

---
+++ search.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file queries the karaoke database by name/artist based on the search term.
+ */
+
+// Connect to the database
+$host = '127.0.0.1';
+$db   = 'karaoke';
+$user = 'karaoke';
+$pass = 'starrtunes';
+$charset = 'utf8';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $opt);
+
+$search = $_GET['search'];
+
+$stmt = $pdo->prepare('SELECT artist, name FROM songs WHERE artist LIKE :search OR name LIKE :search2');
+$stmt->execute(['search' => "%" . $search . "%", 'search2' => "%" . $search . "%"]);
+$resultJSON = json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+echo $resultJSON;
+foreach ($stmt as $row) {
+  echo "<b>" . $row['artist'] . "</b> - " . $row['name'] . "<br />\n";
+}
+echo "done";
